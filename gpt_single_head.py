23b043e2c986388ce799c97e16d31e2580488900
Kermorvant <kermorvant@gmail.com>
2023-01-25 09:47:35 +0100

    questions are finised

--- gpt_single_head.py
+++ gpt_single_head.py
@@ -77,15 +77,16 @@ class Head(nn.Module):
 
     def forward(self, x):
         B,T,C = x.shape
+        ## YOUR CODE HERE
         k = self.key(x)   # (B,T,C)
         q = self.query(x) # (B,T,C)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        weight = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        weight = weight.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        weight = F.softmax(weight, dim=-1) # (B, T, T)
         # perform the weighted aggregation of the values
         v = self.value(x) # (B,T,C)
-        out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+        ###
+        out = weight @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
         return out
 
 
